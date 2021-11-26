150f17c48f28caa4e360b4055c805bd0765d1cfb
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2021-11-26 15:38:50 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,24 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ I am a powerfull file renamer bot created by @pyrogrammers! \nSend my any file and follow the further instructions.
 
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+<b>Please subscribe my youtube channel to support me.</b>
+"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time..."
-    UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
-    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
-    DEL_THUMB = "Thumbnail cleared succesfully!"
-    NO_THUMB = "No thumbnails found!"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    UPGRADE_TEXT = "Enjoy for free."
+    DOWNLOAD_START = "𝙋𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩..."
+    UPLOAD_START = "𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜..."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Please subscribe my youtube channel : https://soo.gd/Ytchannel **"
+    SAVED_THUMB = "Thumbnail saved. it will be permanent until you /delthumb."
+    DEL_THUMB = "Thumbnail removed succesfully!"
+    NO_THUMB = "No thumbnail found!"
+    SAVED_RECVD_DOC_FILE = "File Successfully Downloaded."
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """𝗛𝗲𝗹𝗽 𝘀𝗲𝗰𝘁𝗶𝗼𝗻:
     
 1. Send me any Telegram File.
-2. Choose appropriate option."""
+2. Follow further instructions.
+3. Use /showthumb to get current thumbnail.
+4. Use /delthumb to delete current thumbnail.
+5. Send me any image to set your thumbnail."""
 
